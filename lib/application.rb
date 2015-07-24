b5a81e8acd1cf404c017bad4edcf560cf3c71fa7
tohrinagi <tohrinagi@gmail.com>
2015-07-24 22:20:11 +0900

    add inifiale and ldap.

--- lib/application.rb
+++ lib/application.rb
@@ -15,14 +15,19 @@ module SurrogatorUploader
     end
 
     get '/home' do
+      # LDAPログインできていなければエラーページ
+      # メールアドレスがなければ、すみませんページ
+      # それ以外ならばアイコンがあればアイコン表示。なければデフォルト表示
       haml :home
     end
 
     post '/session' do
+      # LDAPログインをする。ログインできない場合エラー
       redirect '/home'
     end
 
     post '/upload' do
+      # 画像をフォルダに上書きし、php実行
       redirect '/home'
     end
 
